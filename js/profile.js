5dc91c1226091aa3749e6cf7c301b6b11ccac246
JasonYeoh02 <yeoh.shan801@gmail.com>
2024-12-12 17:42:10 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -8,9 +8,8 @@ import {
     collection,
     getDocs,
     updateEmail,
-    reauthenticateWithCredential,
   } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";  
-import { auth, EmailAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { auth, EmailAuthProvider, reauthenticateWithCredential } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 // Firestore initialization
 const db = getFirestore();
